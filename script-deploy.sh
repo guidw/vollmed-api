ab0ddcdeb93a4db33dddd60f50d72e8c74190e9c
guidw <279171742+guidw@users.noreply.github.com>
2026-09-16 18:10:31 -0300

    Update script-deploy.sh

--- script-deploy.sh
+++ script-deploy.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 docker load -i vollmed-api.tar
 
@@ -6,14 +6,14 @@ mv docker-compose-prod.yaml docker-compose.yaml
 
 container_ids=$(docker ps -q)
 
-if [ -z "$conteiner_ids" ]; then
+if [ -z "$container_ids" ]; then
   echo "Não há containers em execução"
 else
   for container_id in $container_ids; do
     echo "Parando container: $container_id"
     docker stop $container_id
   done
-  echo "Todos os container em execução foram parados"
-if
+  echo "Todos os containers em execução foram parados"
+fi
 
 docker compose up -d
